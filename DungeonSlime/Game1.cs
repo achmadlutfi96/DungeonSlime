ea32774a227f3c6a900a796f1ba0f57f6664b8dc
Achmad Lutfi <Alhacker1996@gmail.com>
2026-04-26 18:27:39 +0700

    added load content, rendering sprite, and manipulated spritebatch

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
@@ -7,10 +8,12 @@ namespace DungeonSlime;
 
 public class Game1 : Core
 {
+    // The MonoGame logo texture
+    private Texture2D _logo;
 
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
-        
+
     }
 
     protected override void Initialize()
@@ -23,6 +26,7 @@ public class Game1 : Core
     protected override void LoadContent()
     {
         // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/logo");
 
         base.LoadContent();
     }
@@ -39,10 +43,75 @@ public class Game1 : Core
 
     protected override void Draw(GameTime gameTime)
     {
-        // GraphicsDevice.Clear(Color.CornflowerBlue);
-        GraphicsDevice.Clear(Color.MonoGameOrange);
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        // GraphicsDevice.Clear(Color.MonoGameOrange);
 
         // TODO: Add your drawing code here
+
+        // The bounds of the icon within the texture.
+        Rectangle iconSourceRect = new Rectangle(0, 0, 128, 128);
+
+        // The bounds of the word mark within the texture.
+        Rectangle wordmarkSourceRect = new Rectangle(150, 34, 458, 58);
+
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin(sortMode: SpriteSortMode.FrontToBack);
+
+        // // Draw the logo texture
+        // SpriteBatch.Draw(
+        //     _logo,              // texture
+        //     new Vector2(        // position
+        //         Window.ClientBounds.Width,
+        //         Window.ClientBounds.Height) * 0.5f,
+        //     null,               // sourceRectangle
+        //     Color.White,        // color
+        //     0.0f, //MathHelper.ToRadians(90),               // rotation
+        //     new Vector2(
+        //         _logo.Width,
+        //         _logo.Height
+        //     ) * 0.5f,       // origin
+        //     1.0f,               // scale
+        //     SpriteEffects.None, // effects
+        //     0.0f                // layerDepth
+        // );
+
+        // Draw only the icon portion of the texture.
+        SpriteBatch.Draw(
+            _logo,              // texture
+            new Vector2(        // position
+                Window.ClientBounds.Width,
+                Window.ClientBounds.Height) * 0.5f,
+            iconSourceRect,     // sourceRectangle
+            Color.White,        // color
+            0.0f,               // rotation
+            new Vector2(        // origin
+                iconSourceRect.Width,
+                iconSourceRect.Height) * 0.5f,
+            1.0f,               // scale
+            SpriteEffects.None, // effects
+            1.0f                // layerDepth
+        );
+
+        // Draw only the word mark portion of the texture.
+        SpriteBatch.Draw(
+            _logo,              // texture
+            new Vector2(        // position
+              Window.ClientBounds.Width,
+              Window.ClientBounds.Height) * 0.5f,
+            wordmarkSourceRect, // sourceRectangle
+            Color.White,        // color
+            0.0f,               // rotation
+            new Vector2(        // origin
+              wordmarkSourceRect.Width,
+              wordmarkSourceRect.Height) * 0.5f,
+            1.0f,               // scale
+            SpriteEffects.None, // effects
+            0.0f                // layerDepth
+        );
+
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
+
 
         base.Draw(gameTime);
     }
