a12c4bc864090f9c23f810b4ad43dada3fc94c24
Achmad Lutfi <Alhacker1996@gmail.com>
2026-04-26 15:52:39 +0700

    added monogamelibrary reference project

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,19 +1,16 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace DungeonSlime;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
 
-    public Game1()
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        
     }
 
     protected override void Initialize()
@@ -25,9 +22,9 @@ public class Game1 : Game
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
         // TODO: use this.Content to load your game content here
+
+        base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
@@ -42,7 +39,8 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        // GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.MonoGameOrange);
 
         // TODO: Add your drawing code here
 
